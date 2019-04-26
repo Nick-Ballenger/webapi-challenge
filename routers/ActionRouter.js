795d8fadb9bb4fa51053062789a7fc968e9636ba
Lambda_School_Loaner_85 <lambda_school_loaner_85@Lambda-School-Loaner-85s-MacBook-Pro.local>
2019-04-26 10:43:33 -0500

    Get by IDs got

--- routers/ActionRouter.js
+++ routers/ActionRouter.js
@@ -1,2 +1,43 @@
 const express = require('express');
 const router = express.Router();
+
+const actionDB =require('../data/helpers/actionModel')
+
+//Get list of Actions
+router.get("/", async (req, res) => {
+    
+    try {
+      const actionList = await actionDB.get();
+      res.json(actionList);
+    } 
+    
+    catch (err) {
+      res.status(500).json({ message: `Server error, couldn't retrieve list` });
+    }
+  });
+
+  router.get("/:Id", async (req, res) => {
+    const {ID} = req.params;
+    try {
+      const action = await actionDB.get(ID);
+
+      if (!action) {
+        res.status(404).json({ message: "This is not the action you're looking for. Action not found" });
+      } 
+      
+      else {
+        res.json(action);
+      }
+    } 
+    
+    catch (err) {
+      res
+        .status(500)
+        .json({ message: "Server error, couldn't retrieve action." });
+    }
+  });
+
+
+
+  module.exports = router; 
+
--- routers/ProjectRouter.js
+++ routers/ProjectRouter.js
@@ -2,3 +2,41 @@ const express = require('express');
 const router = express.Router();
 
 const projectDB =require('../data/helpers/projectModel')
+
+//Get List of Projects
+router.get("/", async (req, res) => {
+    
+    try {
+      const projectList = await projectDB.get();
+      res.json(projectList);
+    } 
+    
+    catch (err) {
+      res.status(500).json({ message: `Server error couldn't retrieve list` });
+    }
+  });
+
+  router.get("/:Id", async (req, res) => {
+    const {ID} = req.params;
+    try {
+      const project = await projectDB.get(ID);
+
+      if (!project) {
+        res.status(404).json({ message: "This is not the project you're looking for. project not found" });
+      } 
+      
+      else {
+        res.json(project);
+      }
+    } 
+    
+    catch (err) {
+      res
+        .status(500)
+        .json({ message: "Server error, couldn't retrieve project." });
+    }
+  });
+
+
+
+  module.exports = router; 
